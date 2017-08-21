0bd30933166b364175972fd20bd17e989c58790c
Alexandr <a.sh.10031987@gmail.com>
2017-08-21 20:24:32 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j;
 
+/**
+ * Calculate
+ * 
+ * @author Aleksandr Shigin
+ * @version $Id$
+ * @since 21.08.2017
+ */
+
 public class Calculate {
-	
+	/**
+	 * Main
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	} 
