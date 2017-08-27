97eb8f38b8103d2abf8cf3714c5e369ecf3a0552
Alexandr <a.sh.10031987@gmail.com>
2017-08-27 16:33:42 +0300

    3.2. Положение точки [#188]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,52 @@
+package ru.job4j.condition;
+
+/**
+ * Point.
+ * @author Aleksandr Shigin
+ * @version $Id$
+ * @since 0.1
+ */
+public class Point {
+    /**
+     * Coordinate x.
+     */
+    private int x;
+    /**
+     * Coordinate y.
+     */
+    private int y;
+    /**
+     * Constructor.
+     * @param x - x coordinate
+     * @param y - y coordinate
+     */
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    /**
+     * getX.
+     * @return x
+     */
+    public int getX() {
+        return this.x;
+    }
+    /**
+     * getY.
+     * @return y
+     */
+    public int getY() {
+        return this.y;
+    }
+
+    /**
+     * Function y(x) = a * x + b.
+     * @param a - first value
+     * @param b - second value
+     * @return true or false
+     */
+    public boolean is(int a, int b) {
+        int dy = this.x * a + b;
+        return (dy == this.y) ? true : false;
+    }
+}
