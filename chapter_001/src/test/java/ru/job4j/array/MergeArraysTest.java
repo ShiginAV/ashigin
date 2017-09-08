02e8702a7ed11618d0922b52f247ce457e3e9baf
Alexandr <a.sh.10031987@gmail.com>
2017-09-08 17:42:38 +0300

    Слияние двух отсортированных массивов

---
+++ chapter_001/src/test/java/ru/job4j/array/MergeArraysTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+/**
+ * Merge arrays.
+ * @author Aleksandr Shigin
+ * @version $Id$
+ * @since 0.1
+ */
+public class MergeArraysTest {
+    /**
+     * Merge two arrays.
+     */
+    @Test
+    public void whenMergeTwoArrays() {
+        MergeArrays mergeArrays = new MergeArrays();
+        int[] leftArr = {1, 2, 3};
+        int[] rightArr = {4, 5, 6};
+        int[] resultArr = mergeArrays.merge(leftArr, rightArr);
+        int[] expectArr = {1, 2, 3, 4, 5, 6};
+        assertThat(resultArr, is(expectArr));
+    }
+}
