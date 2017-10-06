7d64cc6aeca2203cfcec25bbd2e42bc6eb5bfc86
Alexandr <a.sh.10031987@gmail.com>
2017-10-06 18:04:38 +0300

    Тестовое задание. [#792]

---
+++ chapter_002/src/main/java/ru/job4j/chess/Board.java
@@ -0,0 +1,105 @@
+package ru.job4j.chess;
+
+import ru.job4j.chess.exceptions.FigureNotFoundException;
+import ru.job4j.chess.exceptions.ImpossibleMoveException;
+import ru.job4j.chess.exceptions.OccupiedWayException;
+/**
+ * Board.
+ * @author Aleksandr Shigin
+ * @version $Id$
+ * @since 0.1
+ */
+public class Board {
+    /*** Array of figures.*/
+    private Figure[] figures = new Figure[32];
+    /*** Position of figure in array.*/
+    private int pos = 0;
+    /**
+     * Get figures array.
+     * @return figures
+     */
+    public Figure[] getFigures() {
+        return this.figures;
+    }
+    /**
+     * Get position of figure in array.
+     * @return position
+     */
+    public int getPos() {
+        return this.pos;
+    }
+    /**
+     * Add figure in array.
+     * @param figure - figure
+     */
+    public void addFigure(Figure figure) {
+        this.figures[pos++] = figure;
+    }
+    /**
+     * Move a figure to the cells.
+     * @param source - source coordinate
+     * @param dist - final coordinate
+     * @return - true or false
+     * @throws ImpossibleMoveException - if figure not move to coordinate
+     * @throws OccupiedWayException - if way of occupied
+     * @throws FigureNotFoundException - if figure not found
+     */
+    public boolean move(Cell source, Cell dist) throws ImpossibleMoveException, OccupiedWayException, FigureNotFoundException {
+        for (int i = 0; i < getPos(); i++) {
+            if (!this.figures[i].getPosition().equals(source)) {
+                throw new FigureNotFoundException("Figure not found!!!");
+            }
+
+            for (int j = 0; j < this.figures[i].way(dist).length; j++) {
+                for (int k = 0; k < getPos(); k++) {
+                    if (this.figures[i].way(dist)[j].equals(this.figures[k].getPosition())) {
+                        throw new OccupiedWayException("Occupied way!!!");
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+//===================== Bishop =====================
+    /*** Bishop.*/
+    public class Bishop extends Figure {
+        /**
+         * Constructor.
+         * @param position - position
+         */
+        public Bishop(Cell position) {
+            super(position);
+        }
+        @Override
+        public Cell[] way(Cell dist) throws ImpossibleMoveException {
+            int xA = super.getPosition().getX();
+            int yA = super.getPosition().getY();
+            int xB = dist.getX();
+            int yB = dist.getY();
+
+            for (int i = 0; i < getPos(); i++) {
+                if (getFigures()[i].getPosition().equals(dist)) {
+                    throw new ImpossibleMoveException("Impossible moving!!!");
+                }
+            }
+            if (Math.abs(xB - xA) == Math.abs(yB - yA)) {
+                Cell[] way = new Cell[Math.abs(xB - xA)];
+
+                for (int i = 0; i < way.length; i++) {
+                    int dx = (xB > xA) ? 1 : -1;
+                    int dy = (yB > yA) ? 1 : -1;
+
+                    Cell cell = new Cell(xA + dx, yA + dy);
+                    way[i] = cell;
+
+                    xA = cell.getX();
+                    yA = cell.getY();
+                }
+                return way;
+            } else {
+                throw new ImpossibleMoveException("Impossible moving!!!");
+            }
+        }
+    }
+}
