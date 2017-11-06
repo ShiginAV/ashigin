c68bf19a7c3804e6995829cd14e963087cea5839
Alexandr <a.sh.10031987@gmail.com>
2017-11-06 17:39:34 +0300

    5.2.1. Реализовать SimpleArray<T> [#156] 1.1

--- chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
+++ chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -1,28 +1,65 @@
 package ru.job4j.generic;
 
-public class SimpleArray<T> {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleArray<T> implements Iterable<T> {
 
     private Object[] objects;
     private int index = 0;
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int position = 0;
+
+            @Override
+            public boolean hasNext() {
+                return objects.length > position;
+            }
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public T next() {
+                try {
+                    return (T) objects[position++];
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
+    }
+
 
     public SimpleArray(int size) {
         this.objects = new Object[size];
     }
 
     public void add(T value) {
-        this.objects[this.index++] = value;
+        if (this.index < this.objects.length) {
+            this.objects[this.index++] = value;
+        }
     }
 
     @SuppressWarnings("unchecked")
     public T get(int position) {
-        return (T) this.objects[position];
+        if (position <= this.index) {
+            return (T) this.objects[position];
+        }
+        return null;
     }
 
     public void update(T value, int position) {
-        this.objects[position] = value;
+        if (position <= this.index) {
+            this.objects[position] = value;
+        }
     }
 
     public void delete(int position) {
-        this.objects[position] = null;
+        if (position < this.index) {
+            System.arraycopy(this.objects, position + 1,
+                             this.objects, position, this.objects.length - position - 1);
+            this.index--;
+        }
     }
 }
