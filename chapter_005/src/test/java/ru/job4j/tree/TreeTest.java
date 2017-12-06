1d0b4d5594b673a5edd43bfe62e432f2d28582d2
Alexandr <a.sh.10031987@gmail.com>
2017-12-06 17:32:20 +0300

    1. Создать элементарную структуру дерева [#1711]

---
+++ chapter_005/src/test/java/ru/job4j/tree/TreeTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.tree;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class TreeTest {
+
+    @Test(expected = NoSuchElementException.class)
+    public void hasNextNextSequentialInvocation() {
+
+        Tree<String> tree = new Tree<>();
+        tree.add("first", "second");
+        tree.add("first", "third");
+        tree.add("fore", "third");
+        tree.add("fore", "five");
+        tree.add("fore", "six");
+        tree.add("six", "seven");
+        tree.add("six", "five");
+
+        Iterator<String> it = tree.iterator();
+
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("first"));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("second"));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("third"));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("five"));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("six"));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("seven"));
+        it.next();
+    }
+}
