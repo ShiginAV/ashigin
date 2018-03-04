2978875078ace1818b8b6d4fdfb3ad455f2b0787
Alexandr <a.sh.10031987@gmail.com>
2018-03-04 13:28:24 +0300

    3. ThreadSafe динамический список

--- chapter_005/src/main/java/ru/job4j/list/SimpleArrayList.java
+++ chapter_005/src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -1,13 +1,18 @@
 package ru.job4j.list;
 
 import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class SimpleArrayList<E> implements Iterable<E> {
+import net.jcip.annotations.ThreadSafe;
 
+@ThreadSafe
+public class SimpleArrayList<E> implements Iterable<E> {
     private Object[] container;
     private int size = 0;
+    private int modCount = 0;
+    private final Object lock = new Object();
 
     public SimpleArrayList() {
         this.container = new Object[10];
@@ -18,25 +23,29 @@ public class SimpleArrayList<E> implements Iterable<E> {
             this.container = new Object[capacity];
         }
     }
-
     // add element
     public void add(E value) {
-        if (size >= container.length) {
-            container = Arrays.copyOf(container, container.length + container.length);
+        synchronized (lock) {
+            modCount++;
+            if (size >= container.length) {
+                container = Arrays.copyOf(container, container.length + container.length);
+            }
+            container[size++] = value;
         }
-        container[size++] = value;
     }
-
     // get element by index
     @SuppressWarnings("unchecked")
-    public E get(int index) {
-        return (E) container[index];
+    public synchronized E get(int index) {
+        synchronized (lock) {
+            return (E) container[index];
+        }
     }
 
     @Override
     public Iterator<E> iterator() {
         return new Iterator<E>() {
             int position = 0;
+            int expectedModCount = modCount;
 
             @Override
             public boolean hasNext() {
@@ -46,10 +55,14 @@ public class SimpleArrayList<E> implements Iterable<E> {
             @Override
             @SuppressWarnings("unchecked")
             public E next() {
-                if (this.hasNext()) {
-                    return (E) container[position++];
-                } else {
+                if (!this.hasNext()) {
                     throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                synchronized (lock) {
+                    return (E) container[position++];
                 }
             }
         };
--- chapter_005/src/main/java/ru/job4j/list/SimpleLinkedList.java
+++ chapter_005/src/main/java/ru/job4j/list/SimpleLinkedList.java
@@ -1,83 +1,94 @@
 package ru.job4j.list;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class SimpleLinkedList<E> implements Iterable<E> {
+import net.jcip.annotations.ThreadSafe;
 
+@ThreadSafe
+public class SimpleLinkedList<E> implements Iterable<E> {
     private Node<E> first;
     private Node<E> last;
     private int size = 0;
+    private int modCount = 0;
+    private final Object lock = new Object();
 
     public void add(E value) {
-        Node<E> tmp = last;
-
-        Node<E> newNode = new Node<>(value, tmp, null);
-        last = newNode;
-
-        if (first == null) {
-            first = newNode;
-        } else {
-            tmp.next = newNode;
+        synchronized (lock) {
+            modCount++;
+            Node<E> tmp = last;
+            Node<E> newNode = new Node<>(value, tmp, null);
+            last = newNode;
+            if (first == null) {
+                first = newNode;
+            } else {
+                tmp.next = newNode;
+            }
+            size++;
         }
-        size++;
     }
 
     public E get(int index) {
-        if (index >= size) {
-            throw new IndexOutOfBoundsException();
+        synchronized (lock) {
+            if (index >= size) {
+                throw new IndexOutOfBoundsException();
+            }
+            Node<E> node = first;
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+            return node.item;
         }
-        Node<E> node = first;
-        for (int i = 0; i < index; i++) {
-            node = node.next;
-        }
-        return node.item;
     }
 
-
-
     public E deleteLast() {
-        if (last == null) {
-            throw new NoSuchElementException();
+        synchronized (lock) {
+            if (last == null) {
+                throw new NoSuchElementException();
+            }
+            Node<E> element = last;
+            E item = last.item;
+            last.item = null;
+            if (last.prev == null) {
+                last = null;
+                first = null;
+            } else {
+                last = element.prev;
+                last.next = null;
+                element.prev = null;
+            }
+            size--;
+            return item;
         }
-        Node<E> element = last;
-        E item = last.item;
-        last.item = null;
-        if (last.prev == null) {
-            last = null;
-            first = null;
-        } else {
-            last = element.prev;
-            last.next = null;
-            element.prev = null;
-        }
-        size--;
-        return item;
     }
 
     public E deleteFirst() {
-        if (first == null) {
-            throw new NoSuchElementException();
+        synchronized (lock) {
+            if (first == null) {
+                throw new NoSuchElementException();
+            }
+            Node<E> element = first;
+            E item = first.item;
+            first.item = null;
+            if (first.next == null) {
+                first = null;
+                last = null;
+            } else {
+                first = element.next;
+                first.prev = null;
+                element.next = null;
+            }
+            size--;
+            return item;
         }
-        Node<E> element = first;
-        E item = first.item;
-        first.item = null;
-        if (first.next == null) {
-            first = null;
-            last = null;
-        } else {
-            first = element.next;
-            first.prev = null;
-            element.next = null;
-        }
-        size--;
-        return item;
     }
 
     @Override
     public Iterator<E> iterator() {
         return new Iterator<E>() {
             private int position = 0;
+            int expectedModCount = modCount;
 
             @Override
             public boolean hasNext() {
@@ -86,11 +97,13 @@ public class SimpleLinkedList<E> implements Iterable<E> {
 
             @Override
             public E next() {
-                if (hasNext()) {
-                    return get(position++);
-                } else {
+                if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return get(position++);
             }
         };
     }
